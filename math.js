620cce198e7cc88d9278c41c8e061d9932e1b3e6
bruno049 <j.bruno.che@gmail.com>
2014-05-15 15:17:55 -0500

    Kepler's 2nd Law: A line joining a planet and the Sun sweeps out equal areas during equal intervals of time.

--- math.js
+++ math.js
@@ -7,9 +7,6 @@ Math.ellipse=function(t,o) {
 		y:o.x_radius*Math.cos(2*Math.PI*t/o.time+o.start_angle)*Math.sin(o.rotation)+o.y_radius*Math.sin(2*Math.PI*t/o.time+o.start_angle)*Math.cos(o.rotation)+o.y_offset
 	}
 }
-Math.foci=function(a,b) {
-	return Math.sqrt(Math.pow(Math.max(a,b),2)-Math.pow(Math.min(a,b),2));
-}
 Math.peraph=function(p,a) {
 	return {
 		smaj:(p+a)/2,
@@ -17,6 +14,57 @@ Math.peraph=function(p,a) {
 		smin:.5*(p+a)*Math.sqrt(1-Math.pow((a-p)/(a+p),2)),
 		foci:(a-p)
 	}
+}
+Math.auToKm=function(au) {
+	return au*1.496*Math.ten(8);
+}
+Math.kmToAu=function(km) {
+	return km*6.685*Math.ten(-9);
+}
+Math.der=function(f, x, dx) {
+	return (f(x+(dx || .0000001))-f(x))/(dx || .0000001);
+}
+Math.distance=function(a,b) {
+	return Math.sqrt(Math.pow(a.x-b.x,2)+Math.pow(a.y-b.y,2));
+}
+Math.newtonsMethod=function(c,i,a,b) {
+	for(var k=0;k<i;k++) {
+		c=c-a(c)/(b?b(c):Math.der(a,c));
+	}
+	return c;
+}
+Math.ten=function(p) {
+	return Math.ten(p);
+}
+Math.posRR=function(radius,radian) {
+	return {x:radius*Math.cos(radian),y:radius*Math.sin(radian)}
+}
+Math.G=6.67384*Math.ten(-20);
+Math.keplerianMotion=function(a,t) {
+	/* Mean motion in radians per year */
+	var mm=2*Math.PI/a.time;
+	/* Mean anomaly in radians */
+	var m=(mm*t);
+	/* Eccentric anomaly in radians */
+	var e=Math.newtonsMethod(a.ecc>.8?Math.PI:m,20,function(r){return r-a.ecc*Math.sin(r)-m},function(r){return 1-a.ecc*Math.cos(r)});
+	/* True anomaly in radians */
+	var tr=2*Math.atan2(Math.sqrt(1-a.ecc)*Math.cos(e/2),Math.sqrt(1+a.ecc)*Math.sin(e/2));
+	/* Radius in AU */
+	return {radius:(a.smaj*(1-Math.pow(a.ecc,2)))/(1+a.ecc*Math.cos(tr)),radian:tr};
+}
+jQuery.fn.planetaryMotion=function(x,y,au) {
+	var el=$(this);
+	el.data("keplerianData", {time:0,x_origin:x,y_origin:y,au:au});
+	return setInterval(function(){
+		var k=Math.keplerianMotion($(el).data("planetData"),$(el).data("keplerianData").time);
+		var p=Math.posRR(k.radius*au,k.radian);
+		$(el).css({
+			"position":"absolute",
+			"top":(-p.y+$(el).data("keplerianData").y_origin)+"px",
+			"left":(p.x+$(el).data("keplerianData").x_origin+$(el).data("planetData").foci*au)+"px"
+		});
+		$(el).data("keplerianData").time++;
+	},1);
 }
 jQuery.fn.revolve=function(o) {
 	o=$.extend({
